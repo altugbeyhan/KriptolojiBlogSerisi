e4ee2906f1907e6856d95cec74f3d18c60a579fe
Altuğ Beyhan <54203533+altugbeyhan@users.noreply.github.com>
2024-02-28 00:10:39 +0300

    Create Ders4-Scytale.py

---
+++ Ders4-Scytale.py
@@ -0,0 +1,27 @@
+def skytale_sifreleme(duz_metin,sarim_sayisi):
+    
+    duz_metin = duz_metin.replace(" ","")
+    satir_sayisi = (len(duz_metin)//sarim_sayisi)+1
+    doldurma_sayisi = (satir_sayisi*sarim_sayisi)-len(duz_metin)
+    duz_metin += "X"*doldurma_sayisi
+    sifreli_metin = ""
+    
+    for i in range(sarim_sayisi):
+        for j in range(i,len(duz_metin),sarim_sayisi):
+            sifreli_metin += duz_metin[j]
+    
+    print("ŞİFRELİ METİN:",sifreli_metin)
+
+def skytale_cozme(sifreli_metin,sarim_sayisi):
+    
+    satir_sayisi = len(sifreli_metin)//sarim_sayisi
+    duz_metin = ""
+    
+    for i in range(satir_sayisi):
+        for j in range(i,len(sifreli_metin),satir_sayisi):
+            duz_metin += sifreli_metin[j]
+    
+    print("DÜZ METİN:",duz_metin)
+    
+skytale_sifreleme("GİZLİ BULUŞMA YEDİDE KONGRE MERKEZİNDE",6)
+skytale_cozme("GUYKMİİLEOENZUDNRDLŞİGKEİMDREXBAEEZX",6)
